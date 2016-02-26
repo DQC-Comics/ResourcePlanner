129fef79b2210b4519d7a9019eea054354758574
tobiasferm <tobias.ferm@live.se>
2016-02-26 13:10:36 +0100

    Added angular

--- ResourcePlanner/App_Start/BundleConfig.cs
+++ ResourcePlanner/App_Start/BundleConfig.cs
@@ -21,6 +21,10 @@
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/orderData").Include(
+                      "~/Scripts/orderData.js"));
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                      "~/Scripts/angular.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
---
+++ ResourcePlanner/Scripts/orderData.js
@@ -0,0 +1,24 @@
+﻿var orderData = angular.module('orderData');
+orderData.controller('orderDataController', function ($scope, orders) {
+
+    $scope.orders = [];
+    
+    function populateOrders() {
+        var url = "https://dqccomics-webapi.azurewebsites.net/api/Bookings";
+        $.ajax({
+            url: url,
+            data: data,
+            headers: "application/json",
+            success: function (odata) {
+                console.log(odata)
+            },
+            fail: function (oData) {
+
+            }
+        })
+    }
+    $(document).ready(function () {
+        console.log("Test")
+        populateOrders
+    })
+})
