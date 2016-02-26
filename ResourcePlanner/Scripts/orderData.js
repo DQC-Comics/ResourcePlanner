8c238aceb30314e5d8ebeba81c079b7b1803a29d
tobiasferm <tobias.ferm@live.se>
2016-02-26 23:53:31 +0100

    asd123

--- ResourcePlanner/Scripts/orderData.js
+++ ResourcePlanner/Scripts/orderData.js
@@ -5,10 +5,29 @@ orderData.controller('orderDataController', function ($scope,orders) {
     orders.query(function (data) {
         $scope.orders = data;
     });
+    $scope.openMap = function (coords) {
+        console.log("Coooords")
+       
+    
+    }
+    $scope.pushNotice = function (orderIndex) {
+        console.log("Clicked")
+        console.log(orderIndex)
+        $scope.orders[orderIndex].Status = "Confirmed"
+        $scope.orders[orderIndex].$update(function () {
+            console.log("Item  updated")
+        })
+    }
     
 });
 orderData.factory('orders', function ($resource) {
 
-    return $resource("https://dqccomics-webapi.azurewebsites.net/api/Bookings/:id", { '$orderby': 'id asc' }, {});
+    return $resource("https://dqccomics-webapi.azurewebsites.net/api/Bookings/:id", { id: '@Id' }, {
+        update: {
+            method: 'PUT' // this method issues a PUT request
+        }
+        });
 
 });
+
+
