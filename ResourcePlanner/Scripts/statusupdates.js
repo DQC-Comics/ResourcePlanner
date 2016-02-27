597d7fd67898f47965ac8c0d500ad5a33a79ba60
C.J. Edstam <cjedstam@outlook.com>
2016-02-27 01:53:47 +0100

    kij

---
+++ ResourcePlanner/Scripts/statusupdates.js
@@ -0,0 +1,11 @@
+﻿var resourceHubProxy = $.connection.bookingsHub;
+resourceHubProxy.client.bookingStatus = function (bookingId, status) {
+
+    $('#statusUpdate')[0].innerText = '"Booking with id ' + bookingId + ' is now ' + status + '"';
+    $('#statusUpdate').fadeIn(300).fadeOut(300).fadeIn(500).fadeOut(500).fadeIn(1000);
+};
+
+$.connection.hub.url = "https://dqccomics-webapi.azurewebsites.net/signalR";
+$.connection.hub.start()
+    .done(function () { console.log('Now connected, connection ID=' + $.connection.hub.id); })
+    .fail(function () { console.log('Could not Connect!') });
